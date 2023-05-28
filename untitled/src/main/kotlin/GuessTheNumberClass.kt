fc4cc996c788e240854a8a2e203ac13522172dc7
LilyanDrashanski <lilyandrashanski@gmail.com>
2023-05-28 17:27:47 +0300

    :(

--- untitled/src/main/kotlin/GuessTheNumberClass.kt
+++ untitled/src/main/kotlin/GuessTheNumberClass.kt
@@ -4,22 +4,16 @@ open class GuessTheNumberClass {
         val difficulty = StartGameClass().selectDifficulty()
         val target = (array.indices).random()
     }
-    init {println("Guess the Number")}
+    init {
+        println("Guess the Number")
+    }
     fun guessANumber() {
-        var left = 0
-        var right = array.lastIndex
         for (i in 1..difficulty) {
             val guess = readln().toInt()
-            val mid = left + (right - left) / 2
-
             when {
                 target == guess -> return println("Good job, you guessed the number!")
-                target < guess -> {
-                    left = mid + 1; println("The number is lower!")
-                }
-                else -> {
-                    right = mid - 1;println("The number is higher!")
-                }
+                target < guess -> println("The number is lower!")
+                else -> println("The number is higher!")
             }
         }
         println("Game Over! The number was $target")
